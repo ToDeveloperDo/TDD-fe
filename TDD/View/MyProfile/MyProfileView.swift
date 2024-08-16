5216cc77b35ed648bb9d5ff1fe4071a06634eb1e
ChoiAnYong <dksdyd78@naver.com>
2024-08-16 18:49:53 +0900

    design: MyProfileView 디자인 변경

--- TDD/View/MyProfile/MyProfileView.swift
+++ TDD/View/MyProfile/MyProfileView.swift
@@ -9,49 +9,74 @@ import SwiftUI
 
 struct MyProfileView: View {
     @StateObject var viewModel: MyProfileViewModel
+    @State private var isRefreshing = false
     private let columns = Array(repeating: GridItem(.fixed(170)), count: 2)
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                MyProfileCell(viewModel: viewModel)
-                    .padding(.top, 115)
-                
-                BtnView(viewModel: viewModel)
-                
-                SearchBar(text: $viewModel.searchName) {
+        NavigationView {
+            RefreshableScrollView(isRefreshing: $isRefreshing) {
+                self.refresh()
+            } content: {
+                VStack(spacing: 0) {
+                    MyProfileCell(viewModel: viewModel)
+                        .padding(.top, 115)
+                        .background(Color.fixWh)
+                    
+                    BtnView(viewModel: viewModel)
+                    
+                    SearchBar(text: $viewModel.searchName) {
+                        
+                    }
+                    .padding(.bottom, 14)
+                    .padding(.horizontal, 24)
+                    .background(Color.mainbg)
+                    
+                    if viewModel.users.isEmpty {
+                        VStack {
+                            Spacer()
+                            Text("없음")
+                            Spacer()
+                        }
+                    } else {
+                        LazyVGrid(columns: columns, spacing: 8) {
+                            ForEach(viewModel.users) { user in
+                                UserInfoCardView(user: user) {
+                                    viewModel.accept(id: user.userId)
+                                } action2: {
+                                    viewModel.send(id: user.userId)
+                                }
+                                
+                            }
+                        }
+                        .background(Color.mainbg)
+                    }
+                    
                     
                 }
-                .padding(.bottom, 14)
-                .padding(.horizontal, 24)
-                .background(Color.mainbg)
-                
-                if viewModel.users.isEmpty {
-                    VStack {
-                        Spacer()
-                        Text("없음")
-                        Spacer()
-                    }
-                } else {
-                    LazyVGrid(columns: columns, spacing: 8) {
-                        ForEach(viewModel.users) { user in
-                            UserInfoCardView(user: user) {
-                                viewModel.accept(id: user.userId)
-                            } action2: {
-                                viewModel.send(id: user.userId)
-                            }
-
-                        }
-                    }
-                    .background(Color.mainbg)
-                }
-                
-                
             }
+            .ignoresSafeArea()
+            .background(Color.white.edgesIgnoringSafeArea(.bottom))
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        // TODO: -
+                    }, label: {
+                        Image(.settingIcon)
+                    })
+                }
+            }
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbarBackground(Color.fixWh, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            
+           
+
         }
-        .scrollIndicators(.hidden)
-        .background(Color.fixWh)
-        .background(Color.white.edgesIgnoringSafeArea(.bottom))
+    }
+    private func refresh() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.isRefreshing = false
+        }
     }
 }
 
@@ -117,6 +142,71 @@ private struct BtnView: View {
     }
 }
 
+struct RefreshableScrollView<Content: View>: UIViewRepresentable {
+    let content: Content
+    @Binding var isRefreshing: Bool
+    let onRefresh: () -> Void
+    
+    init(isRefreshing: Binding<Bool>, onRefresh: @escaping () -> Void, @ViewBuilder content: () -> Content) {
+        self._isRefreshing = isRefreshing
+        self.onRefresh = onRefresh
+        self.content = content()
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIScrollView {
+        let scrollView = UIScrollView()
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(context.coordinator, action: #selector(Coordinator.handleRefreshControl), for: .valueChanged)
+        scrollView.refreshControl = refreshControl
+        
+        let hostingController = UIHostingController(rootView: content)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.addSubview(hostingController.view)
+        
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            hostingController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.backgroundColor = UIColor(resource: .mainbg)
+        
+        return scrollView
+    }
+    
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        if isRefreshing {
+            uiView.refreshControl?.beginRefreshing()
+        } else {
+            uiView.refreshControl?.endRefreshing()
+        }
+    }
+    
+    class Coordinator: NSObject {
+        let parent: RefreshableScrollView
+        
+        init(_ parent: RefreshableScrollView) {
+            self.parent = parent
+        }
+        
+        @objc func handleRefreshControl(sender: UIRefreshControl) {
+            if !parent.isRefreshing {
+                parent.isRefreshing = true
+                parent.onRefresh()
+            }
+        }
+    }
+}
 #Preview {
-    MyProfileView(viewModel: .init(users: [.stu1, .stu2, .stu3, .stu4], container: .stub))
+    NavigationStack {
+        MyProfileView(viewModel: .init(users: [.stu1, .stu2, .stu3, .stu4], container: .stub))
+    }
 }
